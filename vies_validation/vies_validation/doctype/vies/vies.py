8eef466e94a9b70a136b966d00ea25c1cd17ef21
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-06-08 08:11:05 +0000

    validate and save vies

--- vies_validation/vies_validation/doctype/vies/vies.py
+++ vies_validation/vies_validation/doctype/vies/vies.py
@@ -3,6 +3,50 @@
 
 # import frappe
 from frappe.website.website_generator import WebsiteGenerator
+import frappe
+import requests
+from xml.etree import ElementTree as ET
 
 class Vies(WebsiteGenerator):
+
+	def validate(self):
+		if self.vies:
+			vat_number = self.vies
+			# Create the SOAP envelope
+			envelope = f"""<?xml version="1.0" encoding="UTF-8"?>
+			<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ec.europa.eu:taxud:vies:services:checkVat:types">
+				<soapenv:Header/>
+				<soapenv:Body>
+					<urn:checkVat>
+						<urn:countryCode>{vat_number[:2]}</urn:countryCode>
+						<urn:vatNumber>{vat_number[2:]}</urn:vatNumber>
+					</urn:checkVat>
+				</soapenv:Body>
+			</soapenv:Envelope>"""
+
+			# Set the headers and body for the POST request
+			headers = {'Content-Type': 'text/xml'}
+			url = 'http://ec.europa.eu/taxation_customs/vies/services/checkVatService'
+
+			# Make the SOAP request using frappe.request
+			try:
+				response = requests.post(url, headers=headers, data=envelope)
+				# Check the response and parse the result
+				if response.status_code == 200:
+					# Extract the valid attribute from the SOAP response
+					root = ET.fromstring(response.content)
+					valid = root.find(".//{urn:ec.europa.eu:taxud:vies:services:checkVat:types}valid").text == "true"
+					if not valid:
+						# VAT number is invalid
+						frappe.throw('Invalid VAT number')
+				else:
+					# Error occurred while validating VAT number
+					frappe.throw('Error occurred while validating VAT number')
+			except Exception as e:
+				frappe.throw('Please try again with a valid VAT number')		
+
+		# Save customer
+		frappe.flags.ignore_permissions = True
+		frappe.db.set_value("Customer", self.customer, "tax_id", self.vies)
+
 	pass
