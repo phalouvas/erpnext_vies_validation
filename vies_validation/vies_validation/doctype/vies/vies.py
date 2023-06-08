5b2641256a31de5c2b33c7f38a9ed857c8d4d807
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-06-08 04:44:47 +0000

    added vies doctype

---
+++ vies_validation/vies_validation/doctype/vies/vies.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, KAINOTOMO PH LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+class Vies(WebsiteGenerator):
+	pass
